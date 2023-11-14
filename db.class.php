f1250949668b70d88cbb4c344ac4d0062d448636
Seabra53156 <128641828+Seabra53156@users.noreply.github.com>
2023-11-14 15:13:55 -0300

    Add files via upload

--- db.class.php
+++ db.class.php
@@ -1,32 +1,36 @@
 <?php
 
-class DB {
-    public function conn(){
-$host = "localhost";
-$dbname = "db_aula_pweb1";
-$user = "root";
-$password = "";
+class DB
+{
 
-try {
-  $conn = new PDO(
-    "mysql:host=$host;dbname=$dbname;",
-     $user, 
-     $password,
-     [
-      PDO::ATTR_ERRMODE,
-      PDO::ERRMODE_EXCEPTION,
-      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" ,
-     ]
-     );
-    //' echo "Connected successfully";
- return $conn;
+    public function conn()
+    {
+        $host = "localhost";
+        $dbname = "db_aula_pweb1";
+        $user = "root";
+        $password = "";
 
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+        try {
+            $conn = new PDO(
+                "mysql:host=$host;dbname=$dbname",
+                $user,
+                $password,
+                [ PDO::ATTR_ERRMODE,
+                  PDO::ERRMODE_EXCEPTION,
+                  PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ]
+            );
+
+           // echo "Connected successfully";
+
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
     }
 
     public function select($nome_tabela){
+        
         $conn = $this->conn();
         $sql = "SELECT * FROM $nome_tabela";
 
@@ -34,66 +38,76 @@ try {
         $st->execute();
 
         return $st->fetchAll(PDO::FETCH_CLASS);
+
     }
 
-    public function find($nome_tabela){
+    public function find($nome_tabela, $id){
+        
         $conn = $this->conn();
-        $sql = "SELECT * FROM $nome_tabela WHERE id = ?";
+        $sql = "SELECT * FROM $nome_tabela Where id = ?";
 
         $st = $conn->prepare($sql);
-        $st->execute();
+        $st->execute([$id]);
 
         return $st->fetchObject();
+
     }
 
     public function insert($nome_tabela, $dados){
+        
         $conn = $this->conn();
-        $sql = "INSERT INTO $nome_tabela(nome, cpf, telefone) 
-                values (?,?,?)";
+        $sql = "INSERT INTO $nome_tabela (nome, cpf, telefone) 
+                    values (?, ?, ? )";
 
         $st = $conn->prepare($sql);
+
         $st->execute([
             $dados['nome'],
-            $dados['cpf'],  
+            $dados['cpf'],
             $dados['telefone'],
         ]);
     }
 
     public function update($nome_tabela, $dados){
+        
         $conn = $this->conn();
-        $sql = "UPDATE $nome_tabela SET nome=?, cpf=?, telefone=?, WHERE id=? "; 
+        $sql = "UPDATE $nome_tabela SET nome=?, cpf=?, telefone=? WHERE id=?";  
+
         $st = $conn->prepare($sql);
+
         $st->execute([
             $dados['nome'],
-            $dados['cpf'],  
+            $dados['cpf'],
             $dados['telefone'],
+            $dados['id'],
         ]);
     }
 
-    
-        public function destroy($nome_tabela, $id){
-            $conn = $this->conn();
-            $sql = "DELETE FROM $nome_tabela where id = ?";
-    
-            $st = $conn->prepare($sql);
-            $st->execute([$id]);
+    public function destroy($nome_tabela, $id){
+        
+        $conn = $this->conn();
+        $sql = "DELETE FROM $nome_tabela where id = ?";
+
+        $st = $conn->prepare($sql);
+        $st->execute([$id]);
 
     }
 
     public function search($nome_tabela, $dados){
+        
+        //var_dump($dados);
+        //exit;
         $campo = $dados['tipo'];
         $valor = $dados['valor'];
-
-
 
         $conn = $this->conn();
         $sql = "SELECT * FROM $nome_tabela where $campo LIKE ?";
 
         $st = $conn->prepare($sql);
         $st->execute(["%$valor%"]);
-        
-        return $st->fetchAll(PDO::FETCH_CLASS);
-}   
 
+        return $st->fetchAll(PDO::FETCH_CLASS);
+    }
 }
+
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php
+ include './db.class.php';
+
+//include "./paginas/cabecalho.inc.php";
+//include "./paginas/menu.inc.php";
+//include "./paginas/conteudo.inc.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fernando</title>
+    <title>Document</title>
 </head>
 <body>
-  <h3>Bem vindo ao sistema academico</h3>
-  <a href="paginas/AlunoList.php">Aluno</a><br>
+  <h3>Bem Vindos ao sistema acadêmico</h3>
+  <a href="paginas/AlunoList.php">Aluno</a>    
 
+<?php
+  //  include "./paginas/rodape.inc.php";
+?>
 </body>
 </html>
